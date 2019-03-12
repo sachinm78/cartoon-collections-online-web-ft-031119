9858d03cd76cadc8ed6737b74e8130f958eb39f3
Sachin Mahajan <sachinm78@gmail.com>
2019-03-12 20:36:20 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,8 +1,8 @@
 def roll_call_dwarves(array)
-  counter = 0 
-  while counter < array.length
-    puts "#{counter + 1}. #{array[i]}"
-    counter += 1
+  i = 0 
+  while i < array.length
+    puts "#{i + 1}. #{array[i]}"
+    i += 1
   end
 end
 
