b2201de90492d53787ddb8a01e1a862b2aa58feb
Sachin Mahajan <sachinm78@gmail.com>
2019-03-12 20:32:03 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,7 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(array)
+  counter = 0 
+  while counter < 1 
+    puts "#{counter}. #{name}"
 end
 
 def summon_captain_planet# code an argument here
